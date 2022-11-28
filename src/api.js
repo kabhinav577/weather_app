bcc0307439bf03755067bdf6b44113efa9f70422
kabhinav577 <kabhinav577@gmail.com>
2022-11-28 11:52:19 -0600

    search components

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8a1432b603mshffdc0801d6c7fc6p1d4e3ajsnb4387bbd53e8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
